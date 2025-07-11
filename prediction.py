8e8799f763d5f9faaa809b635543eeb3f61a37d5
Yashvardhan <yashvardhansingh9532@gmail.com>
2025-07-11 16:00:13 +0530

    modified

--- prediction.py
+++ prediction.py
@@ -1,7 +1,6 @@
 import numpy as np
 import tensorflow as tf
 from keras.models import load_model
-from keras.layers import TextVectorization
 from keras.preprocessing.sequence import pad_sequences
 
 model=load_model('models/lstm_model.keras')
